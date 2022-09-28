450833f063bd1b6ec744a0b62d2d82f549462739
JoaoMartins-Forge <75683555+JoaoMartins-Forge@users.noreply.github.com>
2022-09-28 17:13:53 -0300

    first commit

---
+++ edit2d2da4r/DrawPolyLine/Commands.cs
@@ -0,0 +1,99 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.Creation;
+using Autodesk.Revit.DB;
+using DesignAutomationFramework;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace Autodesk.Forge.Sample.DesignAutomation.Revit
+{
+	[Transaction(TransactionMode.Manual)]
+	[Regeneration(RegenerationOption.Manual)]
+	public class Commands : IExternalDBApplication
+	{
+		//Path of the project(i.e)project where your Window family files are present
+		string OUTPUT_FILE = "OutputFile.rvt";
+
+		public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+		{
+			DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+			return ExternalDBApplicationResult.Succeeded;
+		}
+
+		private void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+		{
+			LogTrace("Design Automation Ready event triggered...");
+			e.Succeeded = true;
+			CreatePolyLine(e.DesignAutomationData.RevitDoc);
+		}
+
+		private void CreatePolyLine(Document revitDoc)
+		{
+			InputParams inputParameters = JsonConvert.DeserializeObject<InputParams>(File.ReadAllText("params.json"));
+
+			using (Transaction trans = new Transaction(revitDoc))
+			{
+				trans.Start("Create Curves");
+
+				Console.WriteLine("Reading input points");
+				List<XYZ> points = inputParameters.Points.Select(point => new XYZ(point.X, point.Y, point.Z)).ToList();
+				Console.WriteLine($"{points.Count} points found!");
+				points.ForEach(point => Console.WriteLine(point.ToString()));
+
+				Console.WriteLine("Acquiring View!");
+				List<Element> views = new FilteredElementCollector(revitDoc).OfClass(typeof(View)).WhereElementIsNotElementType().ToElements().ToList();
+
+				View inputView = views.Find(v => v.Name == inputParameters.ViewName) as View;
+
+				Console.WriteLine($"View {inputView.Name} found!");
+
+				Console.WriteLine("Creating Curves!");
+				for (int i = 0; i < points.Count - 1; i++)
+				{
+					Line newLine = Line.CreateBound(points[i], points[i + 1]);
+					revitDoc.Create.NewModelCurve(newLine, inputView.SketchPlane);
+				}
+				Console.WriteLine("Curves Created!");
+
+				trans.Commit();
+			}
+
+			//Save the updated file by overwriting the existing file
+			ModelPath ProjectModelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(OUTPUT_FILE);
+			SaveAsOptions SAO = new SaveAsOptions();
+			SAO.OverwriteExistingFile = true;
+
+			//Save the project file with updated window's parameters
+			LogTrace("Saving file...");
+			revitDoc.SaveAs(ProjectModelPath, SAO);
+		}
+		public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+		{
+			return ExternalDBApplicationResult.Succeeded;
+		}
+
+		public class InputParams
+		{
+			public string ViewName { get; set; }
+			public List<InputPoint> Points { get; set; }
+		}
+
+		public class InputPoint
+		{
+			public double X { get; set; }	
+			public double Y { get; set; }
+			public double Z { get; set; }
+		}
+
+		/// <summary>
+		/// This will appear on the Design Automation output
+		/// </summary>
+		private static void LogTrace(string format, params object[] args) { System.Console.WriteLine(format, args); }
+	}
+}
+
---
+++ edit2d2da4r/DrawPolyLine/README.md
@@ -0,0 +1,132 @@
+﻿# Revit Materials Extractor bundle for Forge Design Automation
+
+[![Design Automation](https://img.shields.io/badge/Design%20Automation-v3-green.svg)](http://developer.autodesk.com/)
+
+![Windows](https://img.shields.io/badge/Plugins-Windows-lightgrey.svg)
+![.NET](https://img.shields.io/badge/.NET%20Framework-4.8-blue.svg)
+[![Revit-2022](https://img.shields.io/badge/Revit-2022-lightgrey.svg)](http://autodesk.com/revit)
+
+
+# Description
+
+This sample demonstrates a way to draw curves through DA4R
+
+# Development Setup
+
+## Prerequisites
+
+1. **Forge Account**: Learn how to create a Forge Account, activate subscription and create an app at [this tutorial](http://learnforge.autodesk.io/#/account/). 
+2. **Visual Studio 2022** (Windows).
+3. **Revit 2022**: required to compile changes into the plugin
+
+## Design Automation Setup
+
+### Activity example
+
+```json
+{
+  "id": "RevitMaterialsExtractorActivity",
+  "commandLine": [
+    "$(engine.path)\\\\revitcoreconsole.exe /i \"$(args[inputFile].path)\" /al \"$(appbundles[DrawCurves].path)\""
+  ],
+  "parameters": {
+    "inputFile": {
+      "verb": "get",
+      "description": "Input Revit File",
+      "required": true,
+      "localName": "$(inputFile)"
+    },
+    "inputJson": {
+      "zip": false,
+	  "verb": "get",
+	  "description": "Input json",
+	  "required": true,
+	  "localName": "params.json"
+    },
+    "result": {
+      "zip": false,
+      "verb": "put",
+      "description": "Revit model with added curves",
+      "localName": "OutputFile.rvt"
+    }
+  },
+  "engine": "Autodesk.Revit+2022",
+  "appbundles": [
+    "Autodesk.DrawCurves+dev"
+  ],
+  "description": "Draw curves on Revit file using Design Automation"
+}
+```
+
+### Workitem example
+
+```json
+{
+  "activityId": "ID OF THE ACTIVITY",
+  "arguments": {
+    "inputFile": {
+      "url": "URL TO DOWNLOAD THE INPUT FILE (BIM360/ACC)",
+      "Headers": {
+        "Authorization": "Bearer TOKEN"
+      }
+    },
+	"inputJson": {
+	  "verb": "get",
+	  "url": "data:application/json,{
+		  "ViewName": "Nível 1",
+		  "Points": [
+			{
+			  "X": 2.62655557052325,
+			  "Y": 47.8415984391073,
+			  "Z": 0
+			},
+			{
+			  "X": -26.8978097037929,
+			  "Y": 12.6558300543774,
+			  "Z": 0
+			},
+			{
+			  "X": 3.26142034026135,
+			  "Y": -12.6507687521793,
+			  "Z": 0
+			},
+			{
+			  "X": 51.7263688152716,
+			  "Y": -4.1050907173895,
+			  "Z": 0
+			}
+		  ]
+	  }"
+	},
+    "result": {
+      "verb": "post",
+      "url": "URL TO UPLOAD THE RESULT",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    },
+    "onProgress": {
+      "verb": "post",
+      "url": "URL TO RECEIVE PROGRESS INFORMATION",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    },
+    "onComplete": {
+      "verb": "post",
+      "url": "URL TO RECEIVE STATUS AFTER JOB EXECUTION",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    }
+  }
+}
+```
+
+## License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT). Please see the [LICENSE](LICENSE) file for full details.
+
+## Written by
+
+João Martins [@JooPaulodeOrne2](http://twitter.com/JooPaulodeOrne2), [Forge Partner Development](http://forge.autodesk.com)
